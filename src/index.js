519dd494f873fdc3a89ebc9be48f106c0bc5ee5c
Fedulov Ivan <fedulov.ivan@gmail.com>
2017-12-23 14:29:38 +0300

    disables service worker cache

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 
+import { unregister } from './registerServiceWorker';
+unregister();
+
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
+
